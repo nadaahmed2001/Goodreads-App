f8a46ff95f3890f8c8ad4dc9f8c3cb05b8e3b19b
rahmazaki20 <rahmaramahmoud@gmail.com>
2025-02-01 10:03:32 +0200

    Adding Author schema

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
   title: String,
-  author: String,
   category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "Author" },
   description: String,
   coverImage: String,
   price: Number,
