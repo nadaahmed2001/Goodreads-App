ef320abd7e92746778d34f2b1518530a42ac46c6
Nada Ahmed <nada052001@gmail.com>
2025-02-02 12:35:57 +0200

    All books problem solved

--- backend/server.js
+++ backend/server.js
@@ -62,6 +62,20 @@ app.get("/authors/:authorId", async (req, res) => {
   }
 });
 
+app.get("/books/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
+  console.log(`Looking for book with ID: ${bookId}`);
+
+  try {
+    // Populate the author field with the name field from the Author model and populate categoru name
+    const book = await Book.findById(bookId).populate("author", "name").populate("category", "name");
+    res.json(book);
+    console.log("Book fetched successfully from server.js");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // Start the server
 // const PORT = process.env.PORT || 5000;
 const PORT = 5000;
