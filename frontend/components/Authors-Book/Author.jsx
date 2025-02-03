f939a4bea7a3ff858ed25c39fbbf8aecd6457a89
rahmazaki20 <rahmaramahmoud@gmail.com>
2025-02-03 11:13:23 +0200

    adding functionality for AuthorDetails page

--- frontend/components/Authors-Book/Author.jsx
+++ frontend/components/Authors-Book/Author.jsx
@@ -1,50 +1,97 @@
-// import React from 'react'
+// // import React from 'react'
+// // import './Authors-Book.css';
+// // export default function Author() {
+// //   return (
+// //     <>
+// //   <section className="cards">
+// // <div className='card'>
+// // <h3></h3>
+// // <img></img>
+// // <p></p>
+// // </div>
+// //   </section>
+// //     </>
+// //   )
+// // }
+// import React from 'react';
 // import './Authors-Book.css';
-// export default function Author() {
+// import { Link } from 'react-router-dom';
+
+
+
+// export default function Author({ authors }) {
+//   return (
+   
+//     <>
+    
+//     <section className="cards">
+//   {authors.map((author) => (
+//     <div className="card" key={author.id}>
+//       <h3>
+//       {/* <Link to={`/AuthorDetails/${author._id}`}>{author.name}</Link> */}
+//       {/* <Link to={`/AuthorDetails/${author._id}`}>{author.name}</Link> */}
+//       <Link to={`/AuthorDetails/${author._id}`}>{author.name}</Link>
+
+
+//       </h3>
+//       <img src={author.image} alt={author.name} />
+//       <p>{author.bio}</p>
+//     </div>
+//   ))}
+// </section>
+
+
+//     </>
+//   );
+// }
+
+
+
+
+
+
+// import React from 'react';
+// import './Authors-Book.css';
+// import { Link } from 'react-router-dom';
+// export default function Author({ authors }) {
 //   return (
 //     <>
-//   <section className="cards">
-// <div className='card'>
-// <h3></h3>
-// <img></img>
-// <p></p>
-// </div>
-//   </section>
+//       <section className="cards">
+//         {authors.map((author) => (
+//           <div className="card" key={author.id}>
+//             <h3>
+//               <Link to={`/AuthorDetails/${author._id}`}>{author.name}</Link>
+//             </h3>
+//             <img src={author.image} alt={author.name} />
+//             <p>{author.bio}</p>
+//           </div>
+//         ))}
+//       </section>
 //     </>
-//   )
+//   );
 // }
+
+
+
 import React from 'react';
 import './Authors-Book.css';
 import { Link } from 'react-router-dom';
 
-
-
 export default function Author({ authors }) {
   return (
-    // <section className="cards">
-    //   {authors.map((author) => (
-    //     <div className="card" key={author.id}>
-    //       <h3>{author.name}</h3>
-    //       <img src={author.image} alt={author.name} />
-    //       <p>{author.bio}</p>
-    //     </div>
-    //   ))}
-    // </section>
     <>
-    
-    <section className="cards">
-  {authors.map((author) => (
-    <div className="card" key={author.id}>
-      <h3>
-        <Link to={`/AuthorDetails/${author._id}`}>{author.name}</Link>
-      </h3>
-      <img src={author.image} alt={author.name} />
-      <p>{author.bio}</p>
-    </div>
-  ))}
-</section>
-
-{/* <Route path='/AuthorDetails/:id' element={<AuthorsBook />} /> */}
+      <section className="cards">
+        {authors.map((author) => (
+          <div className="card" key={author.id}>
+            <h3>
+              <Link to={`/AuthorDetails/${author._id}`}>{author.name}</Link> {/* Ensure this is correct */}
+            </h3>
+            <img src={author.image} alt={author.name} />
+            <p>{author.bio}</p>
+          </div>
+        ))}
+      </section>
     </>
   );
 }
+
