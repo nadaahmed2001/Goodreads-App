f45210cc179f091c61f98caf07628db636c478c5
rahmazaki20 <rahmaramahmoud@gmail.com>
2025-02-04 09:21:40 +0200

    fix

--- frontend/src/Pages/Books/BookDetails.jsx
+++ frontend/src/Pages/Books/BookDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchBookById, addBookToList } from "../../services/api"; // Add the API function
-
+// import { fetchBookById } from "../../servisces/api";
 import {
   Container,
   Row,
