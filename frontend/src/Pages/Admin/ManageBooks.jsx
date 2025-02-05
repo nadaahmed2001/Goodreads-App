bdcc90e1c6afa222e04ba674a8600dcfcda19b2e
MaNn0 <mlolo80013@gmail.com>
2025-02-05 21:55:47 +0200

    helmet

--- frontend/src/Pages/Admin/ManageBooks.jsx
+++ frontend/src/Pages/Admin/ManageBooks.jsx
@@ -5,7 +5,6 @@ import ModalBtn from "../../assets/Reusable";
 import Placeholder from "react-bootstrap/Placeholder";
 import Modify from './Modify'
 import axios from "axios";
-// import 
 
 export default function Books({ category, author }) {
     const [books, setBooks] = useState([]);
