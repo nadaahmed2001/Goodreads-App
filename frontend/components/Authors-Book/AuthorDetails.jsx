4262f6036a492b617de2acff48103a1d1fdf4a08
rahmazaki20 <rahmaramahmoud@gmail.com>
2025-02-06 01:59:29 +0200

    style Author page

--- frontend/components/Authors-Book/AuthorDetails.jsx
+++ frontend/components/Authors-Book/AuthorDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from '../navbar';
 import './Authors-Book.css';
@@ -9,7 +9,8 @@ export default function AuthorDetails() {
   const [books, setBooks] = useState([]);  // Separate state for books
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+ 
+  
   useEffect(() => {
     const apiUrl = `http://localhost:5000/authors/${authorId}`;
 
@@ -53,33 +54,44 @@ export default function AuthorDetails() {
   return (
     <>
       <Navbar />
+      
       {/* Section to display author details */}
-      <section className="cards">
-        <div className="card">
-          <h3>{author.name}</h3>
-          <h4>{author.bio}</h4> {/* Display bio or any other relevant field */}
+      <section className="author-details">
+        <div className="author-details-card">
+         <div className='img'>
           <img src={author.image} alt={author.name} />
+          </div>
+          <div className='container2'>
+          <h3>{author.name}</h3>
+          <h4>{author.birthDate}</h4>
           <p>{author.bio}</p>
+          </div>
         </div>
       </section>
 
       {/* Section to display books by the author */}
-      <section className="books">
-        <h2>Books by {author.name}</h2>
+       <section className="books">
+        <h2 className='bookname'>Books by {author.name}</h2>
         {books.length > 0 ? (
-          <ul>
+           <ul >
             {books.map((book) => (
               <li key={book._id}>
-                <img src={book.coverImage} alt={book.title} />
-                <h3>{book.title}</h3>
-                <p>{book.description}</p>
-              </li>
-            ))}
+                <img className="bookimg"src={book.coverImage} alt={book.title} />
+               <h3 className='booktitle'>{book.title}</h3>
+              <p className='bookdes'>{book.description}</p>
+              <p className="bookcategory">Category: {book.category?.name}</p>
+              <p className="bookrating">Rating: {book.rating}</p>
+              <p className="bookfeatured">{book.featured ? 'Featured' : 'Not Featured'}</p>
+              <p className="booktrending">{book.trending ? 'Trending' : 'Not Trending'}</p>
+            </li>
+            
+        ))}
           </ul>
-        ) : (
-          <p>No books available for this author.</p>
-        )}
-      </section>
+         ) : (
+           <p>No books available for this author.</p>
+      )}
+     </section> 
+   
     </>
   );
 }
