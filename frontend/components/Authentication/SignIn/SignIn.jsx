8a05776268ef61486dd79a36f66ba92d036e48e9
HossamZakaria <hosamking2122@gmail.com>
2025-02-06 23:18:11 +0200

    pull request

--- frontend/components/Authentication/SignIn/SignIn.jsx
+++ frontend/components/Authentication/SignIn/SignIn.jsx
@@ -34,7 +34,7 @@ export default function SignIn() {
 
     axios.post('http://localhost:5000/login', { email, password, role })
     .then(result => {
-      console.log("Login Response:", result); // Debugging
+      // console.log("Login Response:", result); // Debugging
       if (result.data.message === "success") {
         if (rememberMe) {
           localStorage.setItem('token', result.data.token);
