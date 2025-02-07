8724fbfa5ab135bf722ac5fac4479978ce2a6af4
MaNn0 <mlolo80013@gmail.com>
2025-02-07 03:58:32 +0200

    stripe

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -9,10 +9,9 @@ import {
   Moon,
   ShoppingCart,
 } from "lucide-react";
+// import CheckoutButton from "./CheckoutButton"; // Import CheckoutButton
 import "./Navbar.css";
-// import { Helmet } from "react-helmet";
-// import "./Navbar.css";
-
+import CheckoutButton from "../src/Pages/Payment/CheckoutButton";
 
 const Navbar = () => {
   const [user, setUser] = useState(null); // State for user data from backend
@@ -41,70 +40,61 @@ const Navbar = () => {
     sessionStorage.removeItem("token");
     navigate("/sign-in");
   };
+
   return (
     <>
       <nav>
-        <div className='nav-top'>
-          <div className='imgLogo'>
-            <img src='../BookAppLogo.png' alt='logo' />
+        <div className="nav-top">
+          <div className="imgLogo">
+            <img src="../BookAppLogo.png" alt="logo" />
           </div>
-          <div className='Searchinput'>
+          <div className="Searchinput">
             <input
-              type='search'
-              placeholder='Search titles, authors, publishers...'
-              className='form-control search-input'
+              type="search"
+              placeholder="Search titles, authors, publishers..."
+              className="form-control search-input"
             />
           </div>
-          <div className='d-flex align-items-center icons'>
-            <button className='btn text-white'>
+          <div className="d-flex align-items-center icons">
+            <button className="btn text-white">
               <Globe size={20} />
             </button>
-            <button className='btn text-white'>
+            <button className="btn text-white">
               <Moon size={20} />
             </button>
-            <button className='btn text-white'>
+            <button className="btn text-white">
               <ShoppingCart size={20} />
             </button>
 
             {user ? (
               <>
-                <Link to='/CheckoutButton'>
-                  <button className='button-85' role='button'>
-                    Subscribe
-                  </button>
-                </Link>
+                {/* Use the CheckoutButton here */}
+                <CheckoutButton />
                 {/* My Lists Dropdown */}
-                <Dropdown align='end'>
-                  <Dropdown.Toggle id='dropdown-lists' className='sign-in-btn'>
-                    <span className='me-2 fw-medium'>My Lists</span>
+                <Dropdown align="end">
+                  <Dropdown.Toggle id="dropdown-lists" className="sign-in-btn">
+                    <span className="me-2 fw-medium">My Lists</span>
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                    <Dropdown.Item
-                      onClick={() => navigate("/list/currently_reading")}
-                    >
+                    <Dropdown.Item onClick={() => navigate("/list/currently_reading")}>
                       Currently Reading
                     </Dropdown.Item>
-                    <Dropdown.Item
-                      onClick={() => navigate("/list/want_to_read")}
-                    >
+                    <Dropdown.Item onClick={() => navigate("/list/want_to_read")}>
                       Want to Read
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={() => navigate("/list/read")}>
-                      Read
-                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => navigate("/list/read")}>Read</Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
 
                 {/* User Dropdown */}
-
-                <Dropdown align='end'>
-                  <Dropdown.Toggle id='dropdown-user' className='sign-in-btn'>
-                    <span className='me-2 fw-medium'>{user.first_name}</span>
+                <Dropdown align="end">
+                  <Dropdown.Toggle id="dropdown-user" className="sign-in-btn">
+                    <span className="me-2 fw-medium">{user.first_name}</span>
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                    <Dropdown.Item as={Link} to='/profile'>
+                    <Dropdown.Item as={Link} to="/profile">
                       Profile
                     </Dropdown.Item>
                     <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
@@ -113,41 +103,41 @@ const Navbar = () => {
               </>
             ) : (
               <>
-                <Link to='/sign-up'>
-                  <button className='sign-in-btn'>Sign up</button>
+                <Link to="/sign-up">
+                  <button className="sign-in-btn">Sign up</button>
                 </Link>
-                <Link to='/sign-in'>
-                  <button className='log-in-btn'>Log in</button>
+                <Link to="/sign-in">
+                  <button className="log-in-btn">Log in</button>
                 </Link>
               </>
             )}
           </div>
         </div>
 
-        <div className='NavList'>
+        <div className="NavList">
           <ul>
             <li>
-              <Link to='/' className='hoverlink'>
+              <Link to="/" className="hoverlink">
                 Home
               </Link>
             </li>
             <li>
-              <Link to='/Categories' className='hoverlink'>
+              <Link to="/Categories" className="hoverlink">
                 Category
               </Link>
             </li>
             <li>
-              <Link to='/AuthorsBook' className='hoverlink'>
+              <Link to="/AuthorsBook" className="hoverlink">
                 Authors
               </Link>
             </li>
             <li>
-              <Link to='/AboutUs' className='hoverlink'>
+              <Link to="/AboutUs" className="hoverlink">
                 About Us
               </Link>
             </li>
             <li>
-              <Link to='/TermsConditions' className='hoverlink'>
+              <Link to="/TermsConditions" className="hoverlink">
                 Terms & Conditions
               </Link>
             </li>
