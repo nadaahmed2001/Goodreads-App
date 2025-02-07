7cf427f631e9f70d112f67f94002af5cd518c35e
MaNn0 <mlolo80013@gmail.com>
2025-02-07 02:29:57 +0200

    branch bta3y

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,40 @@ const nodemailer = require("nodemailer");
 const authController = require("./controllers/authencation/authController"); // Import the controller
 const BookAuthor = require("./controllers/authorBookController/BookAuthor");
 const bookID = require("./controllers/getBookbyID/bookID");
+
 const {
   verifyToken,
 } = require("./controllers/authorization/authorizationMiddleware"); // Import verifyToken middleware
 const userProfileController = require("./controllers/userProfileController/userProfile");
 const UserBookList = require("./models/UserBookList");
 // const {allbooks} = require("./controllers/admin/crud");
-const { getBooks } = require("./controllers/admin/Book");
+// const { getBooks } = require("./controllers/admin/Book");
 const { getBookById } = require("./controllers/getBookbyID/bookID");
+const {
+  getBooks,
+  postBook,
+  updateBook,
+  deleteBook,
+} = require("./controllers/admin/Book");
 
+const {
+  getCategories,
+  postCategory,
+  updateCategory,
+  deleteCategory,
+} = require("./controllers/admin/Category");
 
+const {
+  getAuthors,
+  postAuthor,
+  updateAuthor,
+  deleteAuthor,
+} = require("./controllers/admin/Author");
+
+const {
+  CreateCheckout,
+  VerifyPayment,
+} = require("./controllers/Payment/Payment");
 /*reviews fatma*/
 // const reviewRoutes = require("./routes/reviewRoutes");
 // app.use("/api", reviewRoutes);
@@ -186,151 +210,50 @@ app.delete(
 // ======== Category ========
 
 // Post Category through Admin Panel
-app.post("/category", (req, res) => {
-  Category.create(req.body)
-    .then((cat) => {
-      console.log("category added:", cat); // Log full book details
-      res.json(cat); // Send full book object to frontend
-    })
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
+// Post Category through Admin Panel  ^T Execute
+app.post("/category", postCategory);
 
 // Get Category through Admin Panel
-app.get("/categories", async (req, res) => {
-  try {
-    const categories = await Category.find();
-    res.json(categories);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to fetch categories" });
-  }
-});
+app.get("/categories", getCategories);
 // Delete Category through Admin Panel
-app.delete("/category/:id", async (req, res) => {
-  try {
-    const categoryId = req.params.id;
-    const deletedCategory = await Category.findByIdAndDelete(categoryId);
-    if (!deletedCategory) {
-      return res.status(404).json({ error: "Category not found" });
-    }
-    res.json({
-      message: "Category deleted successfully",
-      category: deletedCategory,
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Failed to delete category" });
-  }
-});
+app.delete("/category/:id", deleteCategory);
 
 // Put Category through Admin Panel
 
-app.put("/category/:id", (req, res) => {
-  Category.findByIdAndUpdate(req.params.id, req.body, { new: true }) //first parameter is ID , second parameter is the updated changes , third parameter `new: true` returns updated doc
-    .then((updatedCategory) => {
-      if (!updatedCategory) {
-        return res.status(404).json({ error: "Category not found" });
-      }
-      console.log("Category updated:", updatedCategory);
-      res.json(updatedCategory);
-    })
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
+app.put("/category/:id", updateCategory);
 
 // ======== Book ========
-// Post Book through Admin Panel
-
-app.post("/book", (req, res) => {
-  Book.create(req.body)
-    .then((book) => {
-      console.log("Book added:", book); // Log full book details
-      res.json(book); // Send full book object to frontend
-    })
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
 
 // Get Book through Admin Panel
 app.get("/books", getBooks);
 
+// Post Book through Admin Panel
+app.post("/book", postBook);
+
+// Put Book through Admin Panel
+app.put("/book/:id", updateBook);
+
 // Delete Book through Admin Panel
-
-app.delete("/book/:id", async (req, res) => {
-  try {
-    const bookId = req.params.id;
-    const deletedBook = await Book.findByIdAndDelete(bookId);
-    if (!deletedBook) {
-      return res.status(404).json({ error: "Category not found" });
-    }
-    res.json({
-      message: "Book deleted successfully",
-      category: deletedBook,
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Failed to delete book" });
-  }
-});
-
-app.put("/book/:id", (req, res) => {
-  Book.findByIdAndUpdate(req.params.id, req.body, { new: true }) //first parameter is ID , second parameter is the updated changes , third parameter `new: true` returns updated doc
-    .then((updatedBook) => {
-      if (!updatedBook) {
-        return res.status(404).json({ error: "Bood not found" });
-      }
-      console.log("Book updated:", updatedBook);
-      res.json(updatedBook);
-    })
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
+app.delete("/book/:id", deleteBook);
 
 // ======== Author ========
 
 // Add Author through Admin Panel
-app.post("/author", (req, res) => {
-  Author.create(req.body)
-    .then((author) => {
-      console.log("Author added:", author); // Log full book details
-      res.json(author); // Send full book object to frontend
-    })
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
+app.post("/author", postAuthor);
 
 // Get Author through Admin Panel
-app.get("/authors", async (req, res) => {
-  try {
-    const authors = await Author.find();
-    res.json(authors);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to fetch authors" });
-  }
-});
+app.get("/authors", getAuthors);
 
 // Delete Author through Admin Panel
 
-app.delete("/authorsAdmin/:id", async (req, res) => {
-  try {
-    const authorId = req.params.id;
-    const deletedAuthor = await Author.findByIdAndDelete(authorId);
-    if (!deletedAuthor) {
-      return res.status(404).json({ error: "Author not found" });
-    }
-    res.json({
-      message: "Author deleted successfully",
-      category: deletedAuthor,
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Failed to delete author" });
-  }
-});
+app.delete("/authorsAdmin/:id", deleteAuthor);
 
-app.put("/authorsAdmin/:id", async (req, res) => {
-  Author.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((updatedAuthor) => {
-      if (!updatedAuthor) {
-        return res.status(404).json({ error: "Author not found" });
-      }
-      console.log("Author updated:", updatedAuthor);
-      res.json(updatedAuthor);
-    })
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
+app.put("/authorsAdmin/:id", updateAuthor);
+
+// ============= Payment ==================
+app.post("/create-checkout-session", CreateCheckout);
+
+app.get("/verify-payment", VerifyPayment);
 
 // const PORT = process.env.PORT || 5000;
 const PORT = 5000;
