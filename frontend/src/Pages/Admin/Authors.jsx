aedaa9436957b4999d99e2d3b467dbb01bb46b98
HossamZakaria <hosamking2122@gmail.com>
2025-02-07 11:31:52 +0200

    pull req

--- frontend/src/Pages/Admin/Authors.jsx
+++ frontend/src/Pages/Admin/Authors.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import Table from "react-bootstrap/Table";
--- frontend/src/Pages/Admin/ManageBooks.jsx
+++ frontend/src/Pages/Admin/ManageBooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Sidebar from "./Sidebar";
 import Table from "react-bootstrap/Table";
 import ModalBtn from "../../assets/Reusable";
