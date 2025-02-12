e07d87fe1d528147339740a64cfcbe76a6b3d63f
Fatma Wafy <fatmaa1601@gmail.com>
2025-02-12 16:34:17 +0200

    try to git darkmood icon

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -602,18 +602,13 @@ const Navbar = () => {
           </Form>
 
           {/* Centered Navigation Links */}
-          <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
-          <BSNavbar.Collapse id="basic-navbar-nav" className="nav-links-container">
-            <Nav className="mx-auto navbar-main-links">
-              <Nav.Link as={Link} to="/" className="hoverlink">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                
-                
+          <BSNavbar.Toggle aria-controls='basic-navbar-nav' />
+          <BSNavbar.Collapse
+            id='basic-navbar-nav'
+            className='nav-links-container'
+          >
+            <Nav className='mx-auto navbar-main-links'>
+              <Nav.Link as={Link} to='/' className='hoverlink'>
                 {language === "en" ? "Home" : "الصفحة الرئيسية"}
               </Nav.Link>
               <Nav.Link as={Link} to='/categories-home' className='hoverlink'>
@@ -630,17 +625,19 @@ const Navbar = () => {
               </Nav.Link>
             </Nav>
 
-            <Nav className="ms-auto navbar-right-items">
-              <div className="language-selector">
-                
+            <Nav className='ms-auto navbar-right-items'>
+              <div className=' '>
+                <DarkModeToggle />
+              </div>
+
+              <div className='language-selector'>
                 <select
                   value={language}
                   onChange={(e) => setLanguage(e.target.value)}
                   className='form-select'
                 >
-
-                  <option value="en">EN</option>
-                  <option value="ar">AR</option>
+                  <option value='en'>EN</option>
+                  <option value='ar'>AR</option>
                 </select>
               </div>
 
